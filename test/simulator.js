9c4ae76935fbb3130aa45222d1824431d2cd875f
wanderer <mjbecze@gmail.com>
2015-10-02 21:58:58 +0000

    updated deps

--- test/simulator.js
+++ test/simulator.js
@@ -1,66 +1,66 @@
-const tape = require('tape');
-const DHT = require('../index.js');
-const crypto = require('crypto');
-const async = require('async');
-const port = 30306;
-const numOfNode = 24;
+const tape = require('tape')
+const DHT = require('../index.js')
+const crypto = require('crypto')
+const async = require('async')
+const port = 30306
+const numOfNode = 15
 
-var nodes = [];
+var nodes = []
 
-function setup(cb) {
+function setup (cb) {
   for (var i = 0; i < numOfNode; i++) {
     var dht = new DHT({
       address: '0.0.0.0',
       udpPort: port + i,
       secretKey: crypto.randomBytes(32)
-    });
-    nodes.push(dht);
+    })
+    nodes.push(dht)
   }
 
-  async.each(nodes, function(node, done) {
-    node.bind(node.udpPort, '0.0.0.0', done);
-  }, cb);
+  async.each(nodes, function (node, done) {
+    node.bind(node.udpPort, '0.0.0.0', done)
+  }, cb)
 }
 
-function printNodes() {
-  console.log('------------');
-  nodes.forEach(function(node, i) {
-    console.log(i + ': ' + node.kBucket.count());
-  });
+function printNodes () {
+  console.log('------------')
+  nodes.forEach(function (node, i) {
+    console.log(i + ': ' + node.kBucket.count())
+  })
 }
 
-function connect(cb) {
+function connect (cb) {
   nodes[0].ping({
     address: '0.0.0.0',
     port: port + 1
-  }, cb);
+  }, cb)
 }
 
-function bootStrap(cb) {
-  var bootNodes = nodes.slice(2);
-  var i = 1;
-  async.eachSeries(bootNodes, function(node, done) {
-    printNodes();
+function bootStrap (cb) {
+  var bootNodes = nodes.slice(2)
+  var i = 1
+  async.eachSeries(bootNodes, function (node, done) {
+    printNodes()
     node.bootStrap([{
       address: '0.0.0.0',
       port: port + i
-    }], function() {
+    }], function () {
       setTimeout(done, 100)
     })
-  }, cb);
+  }, cb)
 }
 
-function shutDown(cb){
-  async.each(nodes, function(n, done){
-    n.close(done);
-  }, cb);
+function shutDown (cb) {
+  async.each(nodes, function (n, done) {
+    n.close(done)
+  }, cb)
 }
 
 async.series([
   setup,
   connect,
   bootStrap,
-], function() {
-  printNodes();
-  shutDown();
+], function () {
+  printNodes()
+  shutDown()
 })
